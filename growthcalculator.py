45fe94a99df4b97b894dbd637b35427deb417e12
Enesbil <85970148+Enesbil@users.noreply.github.com>
2021-06-16 02:29:26 +0300

    Add files via upload

---
+++ growthcalculator.py
@@ -0,0 +1,4 @@
+print("Welcome to a very simple money growth calculator")
+monthly_savings = int(input("Please enter the amount of dollars you are planning to save monthly [eg. 500]"))
+total_accumulation = int(input("For how many months are you planning to save for? [Only type the amount as a number eg. 5]"))
+print("According to this very basic calculation, you should accumulate", monthly_savings * total_accumulation, "$")
